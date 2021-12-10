c1495b76112ea449b5757eafa0af690d0fc8d635
Be5yond <beyond147896@126.com>
2021-12-10 18:09:37 +0800

    ⚙️ 增加mock 接口的域名分组管理

--- conf.d/admin/mock.lua
+++ conf.d/admin/mock.lua
@@ -4,9 +4,10 @@
 -- @Version :   1.0
 -- @Contact :   beyond147896@126.com.com
 -- @License :   (C)Copyright 2020-2021, WYY
--- @Desc    :   此模块处理管理mock数据的请求，复用一个nginx location，所以用url path来区分接口管理和接口数据管理
---              path = /admin/mock/ 时查看和编辑接口配置
---              path = /admin/mock/{path} 时查看和编辑对应path的mock数据
+-- @Desc    :   此模块处理管理mock数据的请求
+--              path = /admin/mock/path 管理mock接口配置
+--              path = /admin/mock/host 管理mock域名分组配置
+--              path = /admin/mock/data 管理mock数据
 
 
 local redis = require "resty.redis"
@@ -29,35 +30,121 @@ function response(data)
     return json.encode(resp)
 end
 
+local function _json_decode(str)
+    return json.decode(str)
+end
+
+
+function json_decode( str )
+    local ok, t = pcall(_json_decode, str)
+    if not ok then
+        return nil
+    end
+    return t
+end
+
+function get_path_config(keys)
+    -- 根据输入的redis key查找接口mock配置
+    local list = {}
+    for idx, key in pairs(keys) do
+        local paths, err = rds:smembers(key)
+        for i, path in pairs(paths) do
+            local res, err = rds:hget('config:mock', path)
+            if res ~= ngx.null then
+                data = json_decode(res)
+                if data then
+                    data['path'] = path
+                    table.insert(list, data)
+                end
+            end
+        end
+        -- local res, err = rds:hgetall(key)
+        -- -- 处理config中的所有数据放到table中
+        -- for i=1, #res, 2 do
+        --     data = json_decode(res[i+1])
+        --     if data then
+        --         data['path'] = res[i]
+        --         table.insert(list, data)
+        --     end
+        -- end
+    end
+    return list
+end
+
 
 local method = ngx.req.get_method()
-if ngx.var.uri == '/admin/mock/' then
+-- 管理mock接口配置
+if ngx.var.uri == '/admin/mock/path' then
     -- 返回mock的path列表
     if method == 'GET' then
-        local res, err = rds:hgetall('config:mock')
-        ret = {}
-        -- 处理config中的所有数据放到table中
-        for i=1, #res, 2 do
-            data = json.decode(res[i+1])
-            data['path'] = res[i]
-            table.insert(ret, data)
-        end
+        local chained = ngx.req.get_uri_args()['chained']
+        local text = ngx.req.get_uri_args()['text']
+        local group, domain = string.gmatch(chained, '(%S+),(%S+)')()
+        group = group or '*'
+        domain = domain or '*'
+        local keys, err = rds:keys('config:mock:'..group..':'..domain)
+
+        ret = get_path_config(keys)
         ngx.say(response(ret))
         return
-
     -- 新增或者修改mock配置
     elseif method == 'POST' then
         local body = json.decode(ngx.req.get_body_data())
         local path = body.path
+        local group, domain = string.gmatch(body.chained, '(%S+),(%S+)')()
+        body.chained = nil
+        local ok, err = rds:sadd('config:mock:'..group..':'..domain, path)
         body.path = nil
-        local ok = rds:hset('config:mock', path, json.encode(body))
+        local ok, err = rds:hset('config:mock', path, json.encode(body))
         if not ok then
             ngx.say("failed to record: ", err)
         end
     end
 
-else
-    local path = string.sub(ngx.var.uri, 12) -- uri为/static/mock/...  从12个字符之后的字符串为实际的path
+-- 管理服务的分组与域名
+elseif ngx.var.uri == '/admin/mock/domain' then
+    -- 返回服务域名列表
+    if method == 'GET' then
+        local level = ngx.req.get_uri_args()['level']
+        -- 第一层级联选项，返回项目分组
+        if level == '' then
+            local res, err = rds:smembers('config:mock:groups')
+            table.sort(res)
+            ret = {}
+            for k, v in pairs(res) do
+                item = {label=v, value=v}
+                table.insert(ret, item)
+            end
+            ngx.say(response(ret))
+            return
+        -- 第二层级联选线，返回分组下域名列表
+        elseif level == '1' then 
+            local group = ngx.req.get_uri_args()['parentId']
+            local res, err = rds:keys('config:mock:'..group..':*')
+            ret = {}
+            for k, v in pairs(res) do
+                domain = string.gmatch(v, "config:mock:%S+:(%S+)")()
+                item = {label=domain, value=domain}
+                table.insert(ret, item)
+            end
+            ngx.say(response(ret))
+            return
+        else
+            ret = {status=0, data=nil}
+            ngx.say(json.encode(ret))
+            return
+        end
+    -- 新增一个域名
+    elseif method == 'POST' then
+        local body = json.decode(ngx.req.get_body_data())
+        local ok, err = rds:sadd('config:mock:groups', body.group)
+        local ok, err = rds:sadd('config:mock:'..body.group..':'..body.domain, '_')
+    end
+
+-- 管理mock数据
+elseif ngx.var.uri == '/admin/mock/data' then
+    -- local path = string.sub(ngx.var.uri, 12) -- uri为/static/mock/...  从12个字符之后的字符串为实际的path
+    local path = ngx.req.get_uri_args()['path']
     -- 返回对应path的mock数据列表
     if method == 'GET' then
         local res, err = rds:keys('mock:'..path..':*')
@@ -74,18 +161,14 @@ else
         end
         ngx.say(response(ret))
         return
-
-    -- 添加或修改
-    mock数据配置
+    -- 添加或修改mock数据配置
     elseif method == 'POST' then
         local body = json.decode(ngx.req.get_body_data())
         local hash = ngx.md5(table.concat(json.decode(body.data)))
         local key = 'mock:'..path..':'..hash
-        ngx.say(key)
         -- local ok = rds:hset(key, 'data', body.data)
         -- local ok = rds:hset(key, 'resp', body.resp)
         local ok, err = rds:hmset(key, 'data', body.data, 'resp', body.resp, 'switch', body.switch, 'info', body.info, 'delay', body.delay)
-        ngx.say(ok, err)
         ngx.say(response(type(body.switch)))
         return
 
